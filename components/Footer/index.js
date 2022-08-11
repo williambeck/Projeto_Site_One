779de7ef845be14b6b470832532bdbbf93299721
unknown <willbeckcg@gmail.com>
2022-08-11 07:48:21 -0300

    inicial

--- components/Footer/index.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return(
-        <div classname='footer'>
-            <p>Copyright &copy; 2022</p>
-        </div>
-    )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import Footer from '../components/Footer'
+
+
+import styled from "styled-components"
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.colors.primary};
+`;
 
 export default function Home() {
   return (
-    <div >
-      <Footer />
+    <div><Title>Estrutura Next e Lib ( styled components )</Title>
+
     </div>
   )
 }
